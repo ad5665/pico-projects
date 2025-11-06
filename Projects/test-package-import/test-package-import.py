2d7d7eb3c21aa34fd142553fe329998f12e26d84
Adam Charlton <ad5665@live.co.uk>
2025-11-06 09:28:25 +0000

    temp-humidity: update temp package hostname

--- Projects/test-package-import/test-package-import.py
+++ Projects/test-package-import/test-package-import.py
@@ -3,7 +3,7 @@ import screen
 from utils import connect, ifconfig
 from utils.wifiSecrets import WIFI_SSID, WIFI_PASS
 
-wlan = connect(WIFI_SSID, WIFI_PASS, hostname="pico-temp")
+wlan = connect(WIFI_SSID, WIFI_PASS, hostname="pico-test")
 print("Wi-Fi:", ifconfig(wlan))
 
 #screen.lcd.putstr("Simples")
