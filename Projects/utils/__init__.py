9656df7d4415666170fa61cda2429e34d0d70119
Adam Charlton <ad5665@live.co.uk>
2025-11-06 09:16:24 +0000

    temp-humidity: change init to keep alex happy

--- Projects/utils/__init__.py
+++ Projects/utils/__init__.py
@@ -1,6 +1,9 @@
-from utils.wifi import connect, ensure_connected, ifconfig
-from utils.wifiSecrets import *
+#from utils.wifi import connect, ensure_connected, ifconfig
+#from utils.wifiSecrets import *
+#
+## Connect tp Wi-Fi up
+#wlan = wifi.connect(wifiSecrets.WIFI_SSID, wifiSecrets.WIFI_PASS, hostname="pico-motion")
+#print("Wi-Fi:", wifi.ifconfig(wlan))
 
-# Connect tp Wi-Fi up
-wlan = wifi.connect(wifiSecrets.WIFI_SSID, wifiSecrets.WIFI_PASS, hostname="pico-motion")
-print("Wi-Fi:", wifi.ifconfig(wlan))
+from .wifi import connect, ensure_connected, ifconfig
+__all__ = ["connect", "ensure_connected", "ifconfig"]
